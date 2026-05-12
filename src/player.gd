e7a4db5929fc3980d92fefa9ce3466f90208f30c
Helia8 <126818312+Helia8@users.noreply.github.com>
2026-05-12 03:22:26 +0200

    feat: anim switcher

--- src/player.gd
+++ src/player.gd
@@ -1,32 +1,42 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 @export var hp = 1
 
-func _process(delta: float) -> void:
-	if hp <= 0:
-		queue_free()
-		
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		anim.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	_update_animation()
+
+func _update_animation() -> void:
+	if velocity.x != 0:
+		anim.play("walk")
+	return;
+	if not is_on_floor():
+		anim.play("jump")
+	elif velocity.x != 0:
+		anim.play("walk")
+	else:
+		anim.play("idle")
+
+func _process(delta: float) -> void:
+	if hp <= 0:
+		queue_free()
 
 func hit() -> void:
 	hp -= 1
