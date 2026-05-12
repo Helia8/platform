9d9bf778f9af24ad7a0ac2105feaff3136a1391f
Helia8 <126818312+Helia8@users.noreply.github.com>
2026-05-12 03:29:30 +0200

    fix: flipped sprite direction

--- src/player.gd
+++ src/player.gd
@@ -16,7 +16,7 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
-		anim.flip_h = direction < 0
+		anim.flip_h = direction > 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
