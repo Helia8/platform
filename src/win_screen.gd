6035d45a420897d42d6ade302274d5d6fb649094
A4 <flowgeneration1@gmail.com>
2026-05-13 02:09:45 -0500

    feat: winscreen fixed

--- src/win_screen.gd
+++ src/win_screen.gd
@@ -3,7 +3,7 @@ extends Node2D
 @onready var anim: AnimatedSprite2D = $WinScreenAnimation
 func start():
 	anim.visible = true
-	anim.play("default")
+	anim.play("start")
 
 func _process(delta: float):
 	if (!anim.is_playing() and anim.visible):
