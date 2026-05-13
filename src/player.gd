a9b69e9bd38812c6d17f019b9f54f19f8641b07e
Helia8 <126818312+Helia8@users.noreply.github.com>
2026-05-13 21:19:24 +0200

    ..

--- src/player.gd
+++ src/player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 const SPEED = 400.0
 const JUMP_VELOCITY = -700.0
+const FALL_GRAVITY = 1000.0
+const JUMP_GRAVITY = -700.0
 @export var hp: int = 1
 @onready var winScreen: Node2D = $WinScreen
 @onready var deathScreen: Node2D = $DeathScreen
@@ -15,14 +17,15 @@ func _physics_process(delta: float) -> void:
 	if not running:
 		return
 	if not is_on_floor():
+		velocity.y += FALL_GRAVITY * delta
 		velocity += get_gravity() * delta
 
-	if (Input.is_action_just_pressed("ui_accept") and is_on_floor()) or bounce:
+	if (Input.is_action_just_pressed("jump") and is_on_floor()) or bounce:
 		velocity.y = JUMP_VELOCITY
 		velocity.x = platVel.x
 		bounce = false
 
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 		anim.flip_h = direction > 0
